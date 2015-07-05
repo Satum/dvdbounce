93e4dee1758b67980514e7fb6bca32ebdd27883b
Oliver Owens <oli.owens@foo.com>
2015-07-05 15:15:56 +1000

    testing html routes

--- routes/bounce.js
+++ routes/bounce.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('bounce', { title: 'DVD Bounce' });
+  res.render('testbounce');
 });
 
 module.exports = router;
