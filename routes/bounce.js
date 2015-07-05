16e0ff39240aa9411a7f1e08fe333b8b0a619209
Oliver Owens <oli.owens@foo.com>
2015-07-05 15:07:40 +1000

    added bounce.jade

--- routes/bounce.js
+++ routes/bounce.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DVD Bounce' });
+  res.render('bounce', { title: 'DVD Bounce' });
 });
 
 module.exports = router;
