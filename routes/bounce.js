09c3768fed6d5cb6990b43b1ce74032c17d0ee07
Oliver Owens <oli.owens@foo.com>
2015-07-07 15:52:51 +1000

    edditing root route

--- routes/bounce.js
+++ routes/bounce.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('testbounce');
+  res.sendfile('./public/testbounce.html');
 });
 
 module.exports = router;
